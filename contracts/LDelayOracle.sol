cd0d4788b59b2ef48df42726c22351d857131590
Denton24646 <Denton24646@gmail.com>
2018-07-29 15:34:39 -0400

    first lambda implementation using oraclize
    first lambda implementation using oraclize

---
+++ contracts/LDelayOracle.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.4.24;
+
+import "./Oraclize.sol";
+
+// use oraclize to hit custom AWS endpoint - returns string of train status after querying MTA GTFS feed and deserializing result in a lambda function
+// https://lchink7hq2.execute-api.us-east-2.amazonaws.com/Live/
+
+contract LDelayOracle is usingOraclize {
+    string public mtaFeedAPIresult;
+
+    event LTrainStatusUpdate(string result);
+
+    function __callback(bytes32 myid, string result) {
+        if (msg.sender != oraclize_cbAddress()) revert();
+        mtaFeedAPIresult = result;
+        emit LTrainStatusUpdate(result);
+    }
+
+    function getLTrainStatus() payable {
+        oraclize_query("URL", "https://lchink7hq2.execute-api.us-east-2.amazonaws.com/Live/");
+    }
+}
--- contracts/LDelayPayout.sol
+++ contracts/LDelayPayout.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-//pay coverage to beneficiary here
+//pay coverage to beneficiary here to improve contract security
 
 import "./LDelayBase.sol";
 
